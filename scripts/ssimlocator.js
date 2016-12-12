a3f1f14e34ce6d1fff5bcc64f40f118795a6baf9
Patrick McVey <patrickvmcvey@gmail.com>
2016-12-12 13:21:39 -0500

    New layout and map information box. Sponsors included at the bottom of the page.

--- scripts/ssimlocator.js
+++ scripts/ssimlocator.js
@@ -2,16 +2,30 @@
 $(document).ready(function() {
 	$("button").click(function(){
 	    if (this.name == "Ottawa") {
+	    	// Validate date range
+	    	var date = [];
+	    	$.each($("input[name='daterange']"), function(){            
+		    	date =($(this).val()).split(" - ");
+		    });
+		    var jStartDate = new Date(date[0]);
+		    var jEndDate = new Date(date[0]);
+		    // Check intial date
+		    if (jStartDate < new Date("12/8/2016 11:00 AM")) {
+		    	jStartDate = new Date("12/8/2016 11:00 AM");
+		    }
+		    // Check end date
+		    if (jEndDate > new Date()) {
+		    	jEndDate = new Date();
+		    }
+		    // Get data selectec 
 		    var data = [];
-		    var date = [];
 		    var city = this.name;
 		    $.each($("input[name='value']:checked"), function(){            
 		    	data.push($(this).val());
 		    });
-		    $.each($("input[name='daterange']"), function(){            
-		    	date.push($(this).val());
-		    });
-	    	location.href = "/dashboard?" + "*&*" + city + "*&*" + data.join("*,*") + "*&*" + date.join("_");
+		    
+	    	location.href = "/dashboard?" + "location=" + city + "*&*date=" + jStartDate + "*,*" + jEndDate + "*&*data=" + data.join("*,*");
 	    } 
 	});
+
 });
